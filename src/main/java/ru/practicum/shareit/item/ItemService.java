85786e7a8afddf2b365d4a34e61de00445c8527b
Andre <andrey-k10@yandex.ru>
2026-01-27 22:33:09 +0400

    Спринт 15. add-bookings. Доработка  ItemService

--- src/main/java/ru/practicum/shareit/item/ItemService.java
+++ src/main/java/ru/practicum/shareit/item/ItemService.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 public interface ItemService {
     ItemDto addItem(Long userId, ItemDto itemDto);
+
     ItemDto updateItem(Long userId, Long itemId, ItemDto itemDto);
 
     ItemDtoWithBookings getItem(Long itemId);
